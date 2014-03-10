b45ac03265041e63598955b36cb554383832eb9c
Black <gblack@mitre.org>
2014-03-10 12:51:16 -0400

    Updated MeasureAttribute model to contain 2.1 data

--- lib/hqmf-model/attribute.rb
+++ lib/hqmf-model/attribute.rb
@@ -1,17 +1,28 @@
 module HQMF
-  
   class Attribute
     include HQMF::Conversion::Utilities
-    attr_reader :id,:code,:value,:unit,:name
+    attr_reader :id,:code,:code_system,:text,:value,:value_type,:value_code,:value_code_system,:value_name,:unit,:name
     # @param [String] id
     # @param [String] code
+    # @param [String] code_system
+    # @param [String] text
     # @param [String] value
+    # @param [String] value_type
+    # @param [String] value_code
+    # @param [String] value_code_system
+    # @param [String] value_name
     # @param [String] unit
     # @param [String] name
-    def initialize(id,code,value,unit,name)
+    def initialize(id,code,code_system,text,value,value_type,value_code,value_code_system,value_name,unit,name)
       @id = id
       @code = code
+      @code_system = code_system
+      @text = text
       @value = value
+      @value_type = value_type
+      @value_code = value_code
+      @value_code_system = value_code_system
+      @value_name = value_name
       @unit = unit
       @name = name
     end
@@ -19,16 +30,21 @@ module HQMF
     def self.from_json(json)
       id = json["id"] if json["id"]
       code = json["code"] if json["code"]
+      code_system = json["code_system"] if json["code_system"]
+      code_system = json["text"] if json["text"]
       value = json["value"] if json["value"]
+      value = json["value_type"] if json["value_type"]
+      value = json["value_code"] if json["value_code"]
+      value = json["value_code_sytem"] if json["value_code_system"]
+      value = json["value_name"] if json["value_name"]
       unit = json["unit"] if json["unit"]
       name = json["name"] if json["name"]
     
-      HQMF::Attribute.new(id,code,value,unit,name)
+      HQMF::Attribute.new(id,code,code_system,text,value,value_type,value_code,value_code_system,value_name,unit,name)
     end
   
     def to_json
-      json = build_hash(self, [:id,:code,:value,:unit,:name])
-      json
+      json = build_hash(self, [:id,:code,:code_system,:text,:value,:value_type,:value_code,:value_code_system,:value_name,:unit,:name])
     end
   
   end
